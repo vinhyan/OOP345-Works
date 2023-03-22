a7b2290a44ca06fb39f40073fb821e2c9878b242
Vinh Nhan <vnhan1@myseneca.ca>
2023-03-22 12:32:03 -0400

    Added p2

--- WS08/Database.cpp
+++ WS08/Database.cpp
@@ -1,86 +1,100 @@
-#include <fstream>
-#include "Database.h"
+//#include <fstream>
+//#include <iomanip>
+//#include "Database.h"
 
 
 using namespace std;
 namespace sdds {
-   std::shared_ptr<Database> Database::m_addr{};
+   //std::shared_ptr<Database> Database::m_addr{};
 
-   Database::Database(const std::string& filename) {
-      cout << "[" << this << "]" << " Database(const std::string&)" << endl;
-      m_filename = filename;
+   //Database::Database(const std::string& filename) {
+   //   //hex style for the address
+   //   cout << std::hex << "[" << this << "]" << " Database(const std::string&)" << endl;
+   //   m_filename = filename;
 
-      ifstream file(filename);
-      string tmp{};
-      string token{};
-      size_t pos{};
-      size_t i{};
-      while (getline(file, tmp) && i < ARR_SIZE) {
-         //get keys
-         pos = tmp.find_first_of(" ");
-         token = tmp.substr(0, pos);
-         pos = token.find_first_of("_");
-         token.replace(pos, 1, " ");
-         m_keys[i] = token;
+   //   ifstream file(filename);
+   //   string tmp{};
+   //   string token{};
+   //   size_t pos{};
+   //   size_t i{};
+   //   //could use << 
+   //   while (getline(file, tmp) && i < ARR_SIZE) {
+   //      //get keys
+   //      pos = tmp.find_first_of(" ");
+   //      token = tmp.substr(0, pos);
+   //      pos = token.find_first_of("_");
+   //      token.replace(pos, 1, " ");
+   //      m_keys[i] = token;
 
-         //get values
-         pos = tmp.find_last_of(" ");
-         token = tmp.substr(pos + 1);
+   //      //get values
+   //      pos = tmp.find_last_of(" ");
+   //      token = tmp.substr(pos + 1);
 
-         m_values[i] = token;
+   //      m_values[i] = token;
 
-         i++;
-      }
-      m_num_entry = i;
-   }
+   //      i++;
+   //   }
+   //   m_num_entry = i;
+   //}
 
-   std::shared_ptr<Database> Database::getInstance(const std::string& filename) {
-      shared_ptr<Database> inst(new Database(filename));
+   //std::shared_ptr<Database> Database::getInstance(const std::string& filename) {
 
-      if (!m_addr) {
-         m_addr = inst; //shared pointer allows copy
-      }
+   //   if (!m_addr) {
+   //      shared_ptr<Database> inst(new Database(filename));
+   //      m_addr = inst; //shared pointer allows copy
+   //   }
 
-      return m_addr;
-   }
+   //   return m_addr;
+   //}
 
-   Err_Status Database::GetValue(const std::string& key, std::string& value) const {
-      bool found{};
-      Err_Status status;
-      auto i = 0u;
-      for (; i < ARR_SIZE && !found; i++) {
-         found = m_keys[i] == key;
-      }
+   //Err_Status Database::GetValue(const std::string& key, std::string& value) const {
+   //   bool found{};
+   //   Err_Status status;
+   //   auto i = 0u;
+   //   for (; i < ARR_SIZE && !found; i++) {
+   //      found = m_keys[i] == key;
+   //   }
 
-      if (found && i--) {
-         value = m_values[i];
-         status = Err_Status::Err_Success;
-      }
-      else {
-         status = Err_Status::Err_NotFound;
-      }
-      return status;
-   }
+   //   if (found && i--) {
+   //      value = m_values[i];
+   //      status = Err_Status::Err_Success;
+   //   }
+   //   else {
+   //      status = Err_Status::Err_NotFound;
+   //   }
+   //   return status;
+   //}
 
-   Err_Status Database::SetValue(const std::string& key, std::string& value) {
-      Err_Status status;
+   //Err_Status Database::SetValue(const std::string& key, std::string& value) {
+   //   Err_Status status;
 
-      if (m_num_entry < ARR_SIZE) {
-         m_keys[m_num_entry - 1] = key;
-         m_values[m_num_entry - 1] = value;
-         status = Err_Status::Err_Success;
-      }
-      else {
-         status = Err_Status::Err_OutOfMemory;
-      }
+   //   if (m_num_entry < ARR_SIZE) {
+   //      m_keys[m_num_entry - 1] = key;
+   //      m_values[m_num_entry - 1] = value;
+   //      status = Err_Status::Err_Success;
+   //   }
+   //   else {
+   //      status = Err_Status::Err_OutOfMemory;
+   //   }
 
-      return status;
-   }
+   //   return status;
+   //}
 
-   Database::~Database() {
-      cout << "[" << this << "]" << " ~Database()" << endl;
-      //string bkpFilename = m_filename + ".bkp."
-      //ofstream file("")
-   }
+   //Database::~Database() {
+   //   cout << "[" << this << "]" << " ~Database()" << endl;
+   //   string bkpFilename = m_filename + ".bkp.txt";
+   //   ofstream file(bkpFilename);
 
+   //   if (file) {
+   //      for (auto i = 0u; i < m_num_entry; i++) {
+   //         file << setw(25) << left << m_keys[i]
+   //            << "-> " << m_values[i] << endl;
+   //      }
+
+   //   } else {
+   //      throw "Unable to open file!";
+   //   }
+   //}
+
+   
 }
--- WS08/Database.h
+++ WS08/Database.h
@@ -2,6 +2,8 @@
 #define SDDS_DATABASE_H
 
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 #include <memory>
 #include <string>
 
@@ -14,23 +16,140 @@ namespace sdds {
       Err_NotFound,
       Err_OutOfMemory,
    };
+   //singleton = only one instance of the class the whole program
+   //class Database {
+   //   static std::shared_ptr<Database> m_addr; //reflection
+   //   size_t m_num_entry{};
+   //   std::string m_keys[ARR_SIZE];
+   //   std::string m_values[ARR_SIZE];
+   //   std::string m_filename{};
+   ////protected: or private?
+   //   Database() {};
+   //   Database(const std::string& filename);
+   //public:
+   //   static std::shared_ptr<Database> getInstance(const std::string& filename);
+   //   Err_Status GetValue(const std::string& key, std::string& value) const;
+   //   Err_Status SetValue(const std::string& key, std::string& value);
+   //   ~Database();
 
+   //};
+   template<class T>
    class Database {
-      static std::shared_ptr<Database> m_addr; //reflection
+      inline static std::shared_ptr<Database> m_addr{}; //reflection
       size_t m_num_entry{};
       std::string m_keys[ARR_SIZE];
-      std::string m_values[ARR_SIZE];
+      T m_values[ARR_SIZE];
       std::string m_filename{};
-   //protected: or private?
+      //protected: or private?
       Database() {};
-      Database(const std::string& filename);
+      Database(const std::string& filename) {
+            //hex style for the address
+         std::cout << "[" << this << "]" << " Database(const std::string&)" << std::endl;
+         m_filename = filename;
+
+         std::ifstream file(filename);
+
+         while (m_num_entry < ARR_SIZE && (file >> m_keys[m_num_entry] && file >> m_values[m_num_entry])) {
+            size_t pos = m_keys[m_num_entry].find_first_of("_");
+            if (pos != std::string::npos) 
+               m_keys[m_num_entry].replace(pos, 1, " ");
+            encryptDecrypt(m_values[m_num_entry]);
+            m_num_entry++;
+         }
+      };
+
+      void encryptDecrypt(T& value) {};
+
    public:
-      static std::shared_ptr<Database> getInstance(const std::string& filename);
-      Err_Status GetValue(const std::string& key, std::string& value) const;
-      Err_Status SetValue(const std::string& key, std::string& value);
-      ~Database();
+      static std::shared_ptr<Database> getInstance(const std::string& filename) {
+         if (!m_addr) {
+            std::shared_ptr<Database> inst(new Database<T>(filename));
+            m_addr = inst; //shared pointer allows copy
+         }
+
+         return m_addr;
+
+      }
+      Err_Status GetValue(const std::string& key, T& value) const {
+         bool found{};
+         Err_Status status;
+         auto i = 0u;
+         for (; i < m_num_entry && !found; i++) {
+            found = m_keys[i] == key;
+         }
+
+         if (found && i--) {
+            value = m_values[i];
+            status = Err_Status::Err_Success;
+         }
+         else {
+            status = Err_Status::Err_NotFound;
+         }
+         return status;
+
+
+      };
+      Err_Status SetValue(const std::string& key, const T& value) {
+         Err_Status status;
+
+         if (m_num_entry < ARR_SIZE) {
+            m_keys[m_num_entry - 1] = key;
+            m_values[m_num_entry - 1] = value;
+            status = Err_Status::Err_Success;
+         }
+         else {
+            status = Err_Status::Err_OutOfMemory;
+         }
+
+         return status;
+      };
+
+
+
+      ~Database() {
+         std::cout << "[" << this << "]" << " ~Database()" << std::endl;
+         std::string bkpFilename = m_filename + ".bkp.txt";
+         std::ofstream file(bkpFilename);
+
+         if (file) {
+            for (auto i = 0u; i < m_num_entry; i++) {
+               encryptDecrypt(m_values[i]);
+               file << std::setw(25) << std::left << m_keys[i]
+                  << "-> " << m_values[i] << std::endl;
+            }
+         }
+         else {
+            throw "Unable to open file!";
+         }
+
+      };
 
    };
+
+   template<>
+   void Database<std::string>::encryptDecrypt(std::string& value) {
+      const char secret[]{ "secret encryption key" };
+      for (char& C : value) {
+         for (const char& K : secret) {
+            C = C ^ K;
+         }
+
+      }
+   }
+
+   template<>
+   void Database<long long>::encryptDecrypt(long long& value) {
+      const char secret[]{ "secret encryption key" };
+
+     // value is a long long
+      char* c = reinterpret_cast<char*>(&value);
+
+      for (auto i = 0u; i < sizeof(c); i++ )
+         for (const char& K : secret) {
+            c[i] = c[i] ^ K;
+         }
+   }
+
 
 
 }
