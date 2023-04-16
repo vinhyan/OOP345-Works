c6cad83ccac51b5e9e78cbd8f780d4f430b17cc5
Vinh Nhan <vnhan1@myseneca.ca>
2023-04-16 13:52:47 -0400

    Complete ws9

--- WS09/process_data.h
+++ WS09/process_data.h
@@ -9,6 +9,8 @@
 #include<string>
 #include<fstream>
 #include<functional>
+#include<vector>
+#include<thread>
 
 namespace sdds_ws9 {
 	void computeAvgFactor(const int*, int, int, double&);
@@ -16,8 +18,14 @@ namespace sdds_ws9 {
 	class ProcessData {
 		int total_items{};
 		int* data{};
+		// Following parameters are included to be used for managing multi-threaded 
+		//  computation in the operator() function. 
+		int num_threads{ 0 }; // to hold number of threads 
+		double* averages{ nullptr }; // to hold average factors 
+		double* variances{ nullptr }; // to hold variance factors
+		int* p_indices{ nullptr }; // to hold partitioning indices
 	public:
-		ProcessData(std::string);
+		ProcessData(std::string, int n_threads);
 		ProcessData(const ProcessData&) = delete;
 		ProcessData& operator=(const ProcessData&) = delete;
 		~ProcessData();
